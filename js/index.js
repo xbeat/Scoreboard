1dfca91e86885bf952cf5bbbc4b3a3591031ee69
xbeat <giuseppecanale@Giuseppes-MacBook-Pro.local>
2018-01-30 17:26:34 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -57,3 +57,66 @@ for( let i = 0, il = selects.length; i < il; i++ ){
     	this.style.cursor = "none";
     });
 };
+
+class Timer{
+	constructor( callback ) {
+		this.time = 5400;
+		this.status = 0;
+		this.timer_id;
+		this.callback = callback;
+	};
+
+	start() {
+		if( this.status == 0 ) {
+			this.status = 1;
+			this.timer_id = setInterval( function() {
+				this.time--;
+				this.generateTime();
+				if( typeof( this.callback ) === 'function') this.callback( this.time );
+			}.bind( this ), 1000);
+		};
+	};
+
+	stop() {
+		if( this.status == 1 ){
+			this.status = 0;
+			clearInterval( this.timer_id );
+		};
+	};
+
+	reset()	{
+		this.time = 5400;
+		this.generateTime();
+	};
+
+	getTime() {
+		return this.time;
+	};
+
+	getStatus()	{
+		return this.status;
+	};
+
+	generateTime() {
+		this.second = this.time % 60;
+		this.minute = Math.floor( this.time / 60 ) % 60;
+		this.second = ( this.second < 10 ) ? '0' + this.second : this.second;
+		this.minute = ( this.minute < 10 ) ? '0' + this.minute : this.minute;
+		document.getElementsByClassName('second')[ 0 ].innerText = this.second;
+		document.getElementsByClassName('minute')[ 0 ].innerText = this.minute;
+	};
+}
+
+var timer;
+
+document.addEventListener('DOMContentLoaded', function() {
+	timer = new Timer(
+		function( time ) {
+			if( time <= 0 ) { 
+				timer.stop();
+				alert('time out');
+			};
+		}
+	);
+	timer.reset();
+});
